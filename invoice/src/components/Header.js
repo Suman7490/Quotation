010fa483c494d27a0844cd7c813c9b055c5efb7f
suman7490 <suman.dhangar.135497@gmail.com>
2024-09-11 17:51:30 +0530

    changed navber

--- invoice/src/components/Header.js
+++ invoice/src/components/Header.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from "./KRW.png"
 
 const Header = () => {
 
 
 
     return (
-        <div className='container'>
-            <div className='row'>
-                <div className='col-12'>
-                    <ul className='d-flex list-unstyled'>
-                        <li className='p-2'><Link to='/'>View</Link></li>
-                        <li className='p-2'><Link to="/createinvoice">Create Invoice</Link></li>
-                        {/* <li className='p-2'><Link to="/register">Register</Link></li>
-                        <li className='p-2'><Link to="/login">Login</Link></li> */}
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <Link className="navbar-brand" to="/"><img src={Logo} width="100px" /></Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav m-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/">View <span class="sr-only">(current)</span></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/createinvoice">Create Invoice</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Registration</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
                     </ul>
+                    <form className="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
                 </div>
-            </div>
-      
-        </div>
+            </nav>
+
+        </>
     )
 }
 
