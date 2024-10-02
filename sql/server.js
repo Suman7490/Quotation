85b9a5f38a28a72e079e77cf5c7ffef5bfd46e13
suman7490 <suman.dhangar.135497@gmail.com>
2024-10-02 17:08:18 +0530

    created ssl

--- sql/server.js
+++ sql/server.js
@@ -16,6 +16,9 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true
+    },
     connectTimeout: 10000
 })
 
