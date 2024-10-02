be190ba20c3c03e89261a6fdf8b4a2ca37bae0a1
suman7490 <suman.dhangar.135497@gmail.com>
2024-10-02 15:58:27 +0530

    used connection pool

--- sql/server.js
+++ sql/server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 
 
 const db = mysql.createPool({
+    connectionLimit: 10, 
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
