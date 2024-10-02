6c2688fb45c9c855838835303f06a95c246b8ab1
suman7490 <suman.dhangar.135497@gmail.com>
2024-10-02 17:05:21 +0530

    gghg

--- sql/server.js
+++ sql/server.js
@@ -15,16 +15,10 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    connectTimeout: 10000
 })
 
-console.log({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    PORT: process.env.PORT
-})
 
 
 
