0928b87488ab434f267149e184300f7f7a176ba5
suman7490 <suman.dhangar.135497@gmail.com>
2024-10-08 13:52:44 +0530

    changed database tables

--- database_tables/quotation.sql
+++ database_tables/quotation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 05, 2024 at 11:16 AM
+-- Generation Time: Oct 08, 2024 at 10:18 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -33,15 +33,9 @@ CREATE TABLE `quotation` (
   `email` varchar(255) NOT NULL,
   `gender` varchar(10) NOT NULL,
   `date` date NOT NULL,
-  `designation` varchar(255) NOT NULL,
   `domain` varchar(255) NOT NULL,
-  `entitle` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `quantity` int(11) NOT NULL,
   `total` decimal(10,2) NOT NULL,
-  `discount` decimal(10,2) NOT NULL,
-  `grandTotal` decimal(10,2) NOT NULL,
+  `totalService` int(11) NOT NULL,
   `inputCount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -49,11 +43,11 @@ CREATE TABLE `quotation` (
 -- Dumping data for table `quotation`
 --
 
-INSERT INTO `quotation` (`quotation_id`, `name`, `email`, `gender`, `date`, `designation`, `domain`, `entitle`, `description`, `price`, `quantity`, `total`, `discount`, `grandTotal`, `inputCount`) VALUES
-(37, 'ravi sharma', 'ravi@gmail.com', 'male', '2024-08-14', 'Scholar', 'Life Science', 'NA', 'Thesis', 50000.00, 1, 50000.00, 10000.00, 40000.00, 2),
-(38, 'Zakiya Zeenath ', 'zakiya@gmail.com', 'female', '2024-08-31', 'Research Scholar', 'Electrical engineering ', 'NA', '2 Conference Papers, 2 Research Articles, A Thesis', 150000.00, 1, 150000.00, 20000.00, 130000.00, 3),
-(39, 'suman', 'suman@gmail.com', 'female', '2024-09-02', 'Research Scholar', 'Engineering', 'NA', 'Thesis', 50000.00, 1, 50000.00, 10000.00, 40000.00, 2),
-(40, 'suman', 'suman@gmail.com', 'female', '2024-09-02', 'Research Scholar', 'Management', 'NA', 'Thesis', 100000.00, 1, 100000.00, 40000.00, 60000.00, 2);
+INSERT INTO `quotation` (`quotation_id`, `name`, `email`, `gender`, `date`, `domain`, `total`, `totalService`, `inputCount`) VALUES
+(25, 'suman', 'suman@gmail.com', 'female', '2024-10-04', 'Life Science', 80000.00, 1, 2),
+(26, 'suman', 'suman@gmail.com', 'female', '2024-10-07', 'Engineering', 80000.00, 0, 3),
+(27, 'manu', 'manu@gmail.com', 'female', '2024-10-07', 'Life Science', 130000.00, 0, 2),
+(28, 'dheeraj rajput', 'dheeraj@gmail.com', 'male', '2024-10-07', 'Management', 100000.00, 1, 2);
 
 --
 -- Indexes for dumped tables
@@ -73,7 +67,7 @@ ALTER TABLE `quotation`
 -- AUTO_INCREMENT for table `quotation`
 --
 ALTER TABLE `quotation`
-  MODIFY `quotation_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `quotation_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- database_tables/services.sql
+++ database_tables/services.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 08, 2024 at 10:19 AM
+-- Generation Time: Oct 08, 2024 at 10:18 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -24,38 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `payments`
+-- Table structure for table `services`
 --
 
-CREATE TABLE `payments` (
-  `id` int(9) NOT NULL,
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
   `quotation_id` int(11) NOT NULL,
-  `label` varchar(255) NOT NULL,
-  `dueWhen` varchar(255) NOT NULL,
-  `installmentAmount` int(11) NOT NULL
+  `serviceName` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `discount` int(11) NOT NULL,
+  `grandTotal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `payments`
+-- Dumping data for table `services`
 --
 
-INSERT INTO `payments` (`id`, `quotation_id`, `label`, `dueWhen`, `installmentAmount`) VALUES
-(234, 28, 'FIRST', 'On Advance', 50000),
-(235, 28, 'SECOND', 'On completion', 50000),
-(286, 26, 'FIRST', 'On Advance', 30000),
-(287, 26, 'SECOND', 'On receiving chapter 4 and 6', 30000),
-(288, 26, 'THIRD', 'On completion', 20000),
-(292, 25, 'FIRST', 'On Advance', 40000),
-(293, 25, 'SECOND', 'On completion', 40000);
+INSERT INTO `services` (`id`, `quotation_id`, `serviceName`, `price`, `discount`, `grandTotal`) VALUES
+(241, 28, 'Research Paper', 100000, 50000, 50000),
+(242, 28, 'Book', 80000, 30000, 50000),
+(257, 27, 'Thesis', 150000, 50000, 100000),
+(258, 27, 'Book', 80000, 50000, 30000),
+(276, 26, 'Research Paper', 100000, 50000, 50000),
+(277, 26, 'Book', 80000, 50000, 30000),
+(281, 25, 'Book', 80000, 30000, 50000),
+(282, 25, 'Research Proposal', 50000, 20000, 30000);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `payments`
+-- Indexes for table `services`
 --
-ALTER TABLE `payments`
+ALTER TABLE `services`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -63,10 +65,10 @@ ALTER TABLE `payments`
 --
 
 --
--- AUTO_INCREMENT for table `payments`
+-- AUTO_INCREMENT for table `services`
 --
-ALTER TABLE `payments`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=294;
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=283;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
