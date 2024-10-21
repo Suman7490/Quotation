079949ad6b8d41f5d3fd8f0796984053137259b9
suman7490 <suman.dhangar.135497@gmail.com>
2024-10-21 10:42:51 +0530

    changed icon

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -101,7 +101,7 @@ const Home = () => {
                             <TableCell>
                                 <Icon name='edit' size='large' onClick={() => edit(row.id)} className='text-primary' style={{ cursor: 'pointer' }} />
                                 <Icon name='trash' size='large' onClick={() => deleteQuotation(row.id)} className='text-danger' style={{ cursor: 'pointer' }} />
-                                <Icon name='trash' size='large' onClick={() => pdf(row.id)} className='text-success' style={{ cursor: 'pointer' }} />
+                                <Icon name='eye' size='large' onClick={() => pdf(row.id)} className='text-success' style={{ cursor: 'pointer' }} />
                             </TableCell>
                         </TableRow>
                     })}
