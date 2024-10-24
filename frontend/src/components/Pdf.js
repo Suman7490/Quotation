8d66cb96cfcb2b2c08b6cd589fb84da476d3d362
suman7490 <suman.dhangar.135497@gmail.com>
2024-10-24 14:48:10 +0530

    changed

--- frontend/src/components/Pdf.js
+++ frontend/src/components/Pdf.js
@@ -171,33 +171,31 @@ const Pdf = () => {
                                     </table>
                                 </div>
                             </div>
-                            {/* <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /> */}
-                            <div className='container'>
-                                <div className='row pt-5 mt-5 fixed-bottom flex flex-wrap ml-5 mr-5'>
-                                    <div className='col-md-12'>
-                                        <Table className='border-0'>
-                                            <TableRow>
-                                                <TableCell className='border-0'>
-                                                    <div className='header'>
-                                                        <h3 className='p-3' style={{ backgroundColor: '#7F9D34', width: '50%' }}>PAYMENT METHOD</h3>
-                                                        <p className='m-0'>Account Details: 20005393359</p>
-                                                        <p className='m-0'>IFSC: SBIN0002502</p>
-                                                        <p className='m-0'>Branch: Pratap Bajar Vrindavan Mathura</p>
-                                                    </div>
-                                                    <div className='header mt-4'>
-                                                        <h3 className='p-3' style={{ backgroundColor: '#7F9D34', width: '50%' }}>TERMS AND CONDITION</h3>
-                                                        <p className='m-0'>Please send payment within 30 days of receiving this invoice.</p>
-                                                        <p className='m-0'>There will be a 10% interest charge per month on late invoices.</p>
-                                                    </div>
-                                                </TableCell>
-                                                <TableCell className='border-0 text-center'>
-                                                    <img src={Signature} alt='Signature' style={{ width: '100px' }} />
-                                                    <hr style={{ border: '1px solid black' }} />
-                                                    <h5 className='pt-0 m-0'>AUTHORIZED SIGN</h5>
-                                                </TableCell>
-                                            </TableRow>
-                                        </Table>
-                                    </div>
+                            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+                            <div className='row pt-5 mt-5 fixed-bottom flex flex-wrap ml-5 mr-5'>
+                                <div className='col-md-12'>
+                                    <Table className='border-0'>
+                                        <TableRow>
+                                            <TableCell className='border-0'>
+                                                <div className='header'>
+                                                    <h3 className='p-3' style={{ backgroundColor: '#7F9D34', width: '50%' }}>PAYMENT METHOD</h3>
+                                                    <p className='m-0'>Account Details: 20005393359</p>
+                                                    <p className='m-0'>IFSC: SBIN0002502</p>
+                                                    <p className='m-0'>Branch: Pratap Bajar Vrindavan Mathura</p>
+                                                </div>
+                                                <div className='header mt-4'>
+                                                    <h3 className='p-3' style={{ backgroundColor: '#7F9D34', width: '50%' }}>TERMS AND CONDITION</h3>
+                                                    <p className='m-0'>Please send payment within 30 days of receiving this invoice.</p>
+                                                    <p className='m-0'>There will be a 10% interest charge per month on late invoices.</p>
+                                                </div>
+                                            </TableCell>
+                                            <TableCell className='border-0 text-center'>
+                                                <img src={Signature} alt='Signature' style={{ width: '100px' }} />
+                                                <hr style={{ border: '1px solid black' }} />
+                                                <h5 className='pt-0 m-0'>AUTHORIZED SIGN</h5>
+                                            </TableCell>
+                                        </TableRow>
+                                    </Table>
                                 </div>
                             </div>
                         </div>
